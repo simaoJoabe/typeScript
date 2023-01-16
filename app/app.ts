25e8b8f727557eeeb56343775702e89957181ff2
DelJoabe Simão <simaojoabecosta@hotmail.com>
2023-01-15 21:09:32 -0300

    aula typescript

---
+++ app/app.ts
@@ -0,0 +1,2 @@
+import { Negociacoes } from "./models/Negociacao.js";
+const negociacao  = new Negociacoes(new Date(),20,1000)
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,2 @@
-alert("oi")
+import { Negociacoes } from "./models/Negociacao.js";
+const negociacao = new Negociacoes(new Date(), 20, 1000);
